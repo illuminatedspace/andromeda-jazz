1594678a8efc1912e26a8e5f64c6abb10406a67f
Liz Phillips <lizkristinaphillips@gmail.com>
2019-10-19 12:09:37 -0400

    feat: pass args, create say function, and use pokedex fn

--- chat-bot/index.js
+++ chat-bot/index.js
@@ -1,6 +1,8 @@
+import _ from "lodash";
 import tmi from "tmi.js";
 import dotenv from "dotenv";
 import path from "path";
+import pokedex from "./pokedex";
 dotenv.config({ path: path.resolve("../.env") });
 
 // declare env variables
@@ -20,23 +22,32 @@ const tmiOptions = {
 };
 const tmiClient = new tmi.client(tmiOptions);
 
+const say = (target, message) => {
+  tmiClient.say(target, `${message}`);
+};
+
 // Called every time a message comes in
 const onMessageHandler = (target, context, message, self) => {
   if (self) {
     return;
   } // Ignore messages from the bot
 
+  const sayWithTarget = _.partial(say, target);
+
   // Remove whitespace from chat message
-  const [commandName, ...args] = message.trim().split(" ");
+  const [commandName, ...args] = message
+    .trim()
+    .toLowerCase()
+    .split(" ");
 
   // If the command is known, let's execute it
-  if (commandName === "!dice") {
-    const num = rollDice();
-    tmiClient.say(target, `You rolled a ${num}`);
-    console.log(`* Executed ${commandName} command`);
-  } else if (commandName === "!pokedex") {
-    tmiClient.say(target, `${args}`);
-    console.log(args);
+  // if (commandName === "!dice") {
+  //   const num = rollDice();
+  //   say(target, `You rolled a ${num}`);
+  //   console.log(`* Executed ${commandName} command`);
+  // } else
+  if (commandName === "!pokedex") {
+    pokedex(args, sayWithTarget);
   } else {
     console.log(`* Unknown command ${commandName}`);
   }
