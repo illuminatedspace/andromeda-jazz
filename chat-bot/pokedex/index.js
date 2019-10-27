0dfb96cfb2871fc32b66d9437595292321a76388
Liz Phillips <lizkristinaphillips@gmail.com>
2019-10-26 22:03:34 -0400

    feat: handle no pokemon name passed

--- chat-bot/pokedex/index.js
+++ chat-bot/pokedex/index.js
@@ -1,5 +1,11 @@
+import { getCommandInfo, commands } from "../commandMap";
+
 const pokedex = ([pokemonName], say) => {
-  // handle no pokemon name
+  if (!pokemonName) {
+    console.log("No pokemon given ¯_(ツ)_/¯");
+    say(`You're missing an argument. ${getCommandInfo(commands.pokedex)}`);
+    return null;
+  }
   // call pokeapi to get poke facts
   console.log("saying pokemon name");
   say(`${pokemonName}`);
