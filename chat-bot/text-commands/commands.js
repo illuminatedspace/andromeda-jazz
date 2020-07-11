5a9603a6166cf8b806ddf4ad3ecefef9ea3e7dd6
Liz Phillips <lizkristinaphillips@gmail.com>
2020-07-10 22:04:43 -0400

    feat/chore: file encodings and updated discord command

--- chat-bot/text-commands/commands.js
+++ chat-bot/text-commands/commands.js
@@ -3,30 +3,31 @@ const commandNames = {
   discord: "discord",
   donate: "donate",
   furret: "furret",
-  genius: "genius"
+  genius: "genius",
 };
 
 const commands = [
-  {
-    command: commandNames.charity,
-    message:
-      "All money made during this stream will go to Refugee and Immigrant Center for Education and Legal Services (RAICES), the largest provider of pro-bono legal services to immigrants in Texas. Every dollar raised will help RAICES provide lawyers for children, reunite families, provide social services to immigrants in need, resettle refugees, and more. https://tiltify.com/@illuminatedspace/12hourcharitystream"
-  },
+  // {
+  //   command: commandNames.charity,
+  //   message:
+  //     "All money made during this stream will go to Refugee and Immigrant Center for Education and Legal Services (RAICES), the largest provider of pro-bono legal services to immigrants in Texas. Every dollar raised will help RAICES provide lawyers for children, reunite families, provide social services to immigrants in need, resettle refugees, and more. https://tiltify.com/@illuminatedspace/12hourcharitystream"
+  // },
   {
     command: commandNames.discord,
-    message: "Join our Discord server! https://discord.gg/U8amNwk"
-  },
-  {
-    command: commandNames.donate,
     message:
-      "Donate to RAICES through Tiltify to get your donation alert on stream, pick your donation incentive, and vote for the cross stitch pattern. https://tiltify.com/@illuminatedspace/12hourcharitystream/donate"
+      "Join our Discord server for stream announcements, book club, knitting circle, watch party, and to vote on our next mission! https://discord.gg/U8amNwk",
   },
+  // {
+  //   command: commandNames.donate,
+  //   message:
+  //     "Donate to RAICES through Tiltify to get your donation alert on stream, pick your donation incentive, and vote for the cross stitch pattern. https://tiltify.com/@illuminatedspace/12hourcharitystream/donate"
+  // },
   // turn furrent into overlay of the gif
   {
     command: commandNames.furret,
     message:
-      "https://thumbs.gfycat.com/WellinformedDirectBorderterrier-mobile.mp4"
-  }
+      "https://thumbs.gfycat.com/WellinformedDirectBorderterrier-mobile.mp4",
+  },
 ];
 
 const commandMap = new Map();
